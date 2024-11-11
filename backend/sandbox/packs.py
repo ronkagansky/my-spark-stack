b94135bb12cdb575c8c73732b09c7a79679357be
Shrivu Shankar <shrivu1122@gmail.com>
2024-11-10 16:52:58 -0800

    tweaks

--- backend/sandbox/packs.py
+++ backend/sandbox/packs.py
@@ -30,10 +30,31 @@ Tips:
 - Use react-leaflet for maps
 - Use https://random.imagecdn.app/<width>/<height> for random images
 """.strip(),
-    )
+    ),
+    StackPack(
+        id="nextjs-shadcn",
+        title="Nextjs Shadcn",
+        description="A Nextjs app with Shadcn UI. Best for building a modern web app with a nice UI.",
+        from_registry="ghcr.io/sshh12/prompt-stack-pack-nextjs-shadcn:latest",
+        sandbox_start_cmd="cd /app && if [ ! -d 'frontend' ]; then cp -r /frontend .; fi && cd frontend && npm start",
+        stack_description="""
+You are building a Nextjs app with Shadcn UI.
+
+The user choose to use a Nextjs app with Shadcn UI so avoid adding any additional dependencies unless they are explicitly asked for.
+
+Included:
+- Nextjs (use for routing)
+- `lucide-react` for icons
+- The Nextjs app is already created in /app/frontend (do not run `create-next-app`)
+
+Tips:
+- Use react-leaflet for maps
+- Use https://random.imagecdn.app/<width>/<height> for random images
+""".strip(),
+    ),
 ]
 
-DEFAULT_STACK_PACK_ID = "vanilla-react"
+DEFAULT_STACK_PACK_ID = "nextjs-shadcn"
 
 
 def get_pack_by_id(id: str) -> StackPack:
